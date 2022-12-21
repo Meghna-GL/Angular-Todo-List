9590d99431640426833d6c13afa1efd44e49bcaf
Meghna-GL <meghna.srivastava@globallogic.com>
2022-12-21 19:21:10 +0530

    feat: Add todo component

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/app/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent {
+  todos:Todo[];
+  constructor(){
+    this.todos=[
+      {
+        sno: 3,
+        title: "This is title",
+        desc: "Description",
+        active: true
+      },
+      {
+        sno: 1,
+        title: "This is title 1",
+        desc: "Description 1",
+        active: true
+      },
+      {
+        sno: 2,
+        title: "This is title 2",
+        desc: "Description 2",
+        active: true
+      }
+    ]
+  }
 
 }
